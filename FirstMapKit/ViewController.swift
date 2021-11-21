922a112106f5a5277b981083648de4e22a0ddb1e
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-11-21 21:07:24 +0200

    UI design complete

--- FirstMapKit/ViewController.swift
+++ FirstMapKit/ViewController.swift
@@ -6,14 +6,64 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+
+        
+        
     }
+    
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    // check location services are enabled
+    func checkLocationServices() {
+        
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            
+        } else {
+            // show allert letting the user know they have to turn it on
+        }
+    }
+    
+   
 
 
 }
 
+
+extension ViewController: CLLocationManagerDelegate {
+
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+
+        
+
+
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        <#code#>
+    }
+    
+//    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+//        print("lacation update failed \(error)")
+//    }
+    
+    
+}
