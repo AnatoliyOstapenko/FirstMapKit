db46643444c98a34323432503f5d45384c391d9e
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-11-25 00:11:07 +0200

    add AbsolutelyUselessModel

---
+++ FirstMapKit/AbsolutelyUselessModel.swift
@@ -0,0 +1,12 @@
+//
+//  AbsolutelyUselessModel.swift
+//  FirstMapKit
+//
+//  Created by MacBook on 25.11.2021.
+//
+
+import Foundation
+
+struct AbsolutelyUselessModel {
+    // Bla -bla -bla
+}
